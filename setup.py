1047a9b64088383d1088180068b2edc2a94d408c
Josh Bohde <josh.bohde@gmail.com>
2011-02-17 20:46:15 -0600

    Added setup.py and version info.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-cafe',
+      version='0.1.0',
+      description='Templatetag to serve Coffeescript from Django.',
+      author='Josh Bohde',
+      author_email='josh.bohde@gmail.com',
+      url='https://github.com/joshbohde/django-cafe/',
+      packages=['cafe'],
+)
